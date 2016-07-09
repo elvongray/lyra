dee8e56b9ad8d017ace204624298a296a796978e
Arinde Eniola <eniolaarinde1@gmail.com>
2016-07-09 23:05:24 +0100

    handle the response returned and save to database

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,10 +1,30 @@
-import { post } from 'request'
+import request from 'request';
+import Secret from '../secrets.js';
+
+//Note: don't hardcode the redirect url, also present in login page
+const REDIRECT_URL = 'http://localhost:8000/authenticate';
 
 export default (app) => {
 
   app.get('/authenticate', (req, res) => {
+
+    let { clientID, clientSecret } = Secret;
+
     if (req.query.code) {
-      console.log("xzxzxzx", req.query.code)
+      request.post({
+        url: 'https://api.instagram.com/oauth/access_token',
+        form: {
+          client_id: clientID,
+          client_secret: clientSecret,
+          grant_type: 'authorization_code',
+          redirect_uri: REDIRECT_URL,
+          code: req.query.code
+        }
+      }, function(err, res, body){
+        console.log(body);
+      });
+    } else if (req.query.error) {
+       res.sendFile(path.join(process.cwd(), 'public/index.html'));
     }
   })
 }
